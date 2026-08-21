cf915c842842b0c86ce5d888becda7c14522e367
Muhammad Natiq Zaheer <mq2027474@gmail.com>
2026-08-21 16:36:48 +0500

    SQLite setting added for online demo

--- app/database.py
+++ app/database.py
@@ -6,13 +6,20 @@ import logging
 logger = logging.getLogger(__name__)
 
 # Create SQLAlchemy engine for SQL Server
+# engine = create_engine(
+#     settings.DATABASE_URL,
+#     echo=False,
+#     pool_pre_ping=True,
+#     pool_size=10,
+#     max_overflow=20,
+#     fast_executemany=True
+# )
+
+# SQLite Engine (For Render/Online Demo)
 engine = create_engine(
     settings.DATABASE_URL,
     echo=False,
-    pool_pre_ping=True,
-    pool_size=10,
-    max_overflow=20,
-    fast_executemany=True
+    connect_args={"check_same_thread": False}
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
